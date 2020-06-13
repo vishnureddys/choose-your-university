979a492453d9041998a78db0f1c8f638c01ec184
Vishnu S Reddy <52468309+Vishnu-S-Reddy@users.noreply.github.com>
2020-06-13 19:59:00 +0530

    Created the project.

---
+++ cyu/cyu/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cyu project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cyu.settings')
+
+application = get_asgi_application()
